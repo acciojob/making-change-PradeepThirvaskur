0e150312a284eedefee5538a8dec785a7dc9c959
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-07 17:51:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const makeChange = (c) => {
-  // your name here
+const makeChange = (amount) => {
+  // your code here
+
+        if((typeof amount==="number")&&(amount >0 && amount<100))
+            {
+                const change={ q: 0, d: 0, n: 0, p: 0 }; 
+
+                change.q=Math.floor(amount/25);
+                amount %= 25                                  // long form amount=amount%25;
+
+                change.d=Math.floor(amount/10);
+                amount %=10;
+
+                change.n=Math.floor(amount/5);
+                amount %=5;
+
+                change.p=amount;
+            
+                return change;
+            }
+            else{console.log("Enter the correct amount")}
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+const amount = prompt("Enter amount: ");
+alert(JSON.stringify(makeChange(amount)));
